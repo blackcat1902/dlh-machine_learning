2aced49b5ff915d16814299c8bb22b31fd6fbbfb
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-13 14:24:01 +0000

    mo

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def add_arrays(arr1, arr2):
+    """The Function adds two arrays element-wise."""
+    return (arr1 + arr2)
+arr1 = [1, 2, 3, 4]
+arr2 = [5, 6, 7, 8]
+print( add_arrays(arr1, arr2))
