9f687f18bde00083f5c386c880c999909057ef2e
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-14 20:32:41 +0000

    mo

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -7,14 +7,11 @@ def add_matrices2D(mat1, mat2):
     result = []
 
     for i in range(len(mat1)):
-        if len(mat1(i)) != len(mat2(i)):
+        if len(mat1[i]) != len(mat2[i]):
             return None
             
         a = []
-        for j in range (len(mat1[j]))
+        for j in range (len(mat1[i])):
             a.append(mat1[i][j] + mat2[i][j])
         result.append(a)
     return result
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6], [7, 8]]
-print(add_matrices2D(mat1, mat2))
