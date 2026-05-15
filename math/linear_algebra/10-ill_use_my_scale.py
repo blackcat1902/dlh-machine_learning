f633b9ac405f421f907cc08cf2113dcaa57cb8e2
Fatma <fatma@Fatma-MacBook-Pro.local>
2026-05-15 17:20:01 +0200

    Add documentation for task 10

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
+"""
+Module that provides a function to calculate the shape of a numpy.ndarray
+"""
+
 def np_shape(matrix):
+    """
+    Returns the shape of a numpy.ndarray as a tuple of integers
+    """
     return matrix.shape
