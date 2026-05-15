f4b7f576d906a18732e3d8c507ba64e8fd85cdcd
Fatma <fatma@Fatma-MacBook-Pro.local>
2026-05-15 17:25:47 +0200

    Task 11: transpose matrix

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module that provides a function to transpose a numpy.ndarray
+"""
+
+def np_transpose(matrix):
+    """
+    Returns the transpose of a numpy.ndarray
+    """
+    return matrix.transpose()
