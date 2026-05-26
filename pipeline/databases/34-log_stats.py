9f152d2b66b2f8235981162572bfb02d33110fb1
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-26 15:45:59 +0200

    mo

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
     collection = db.nginx
     #total number of logs 
     total_logs = collection.count_documents({})
-    print(f"{total_logs} logs ")
+    print(f"{total_logs} logs")
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = collection.count_documents({"method" : method})
-        print(f ""\method {method}: {count}")
+        print(f "\tmethod {method}: {count}")
     
     #Get status count
-    status_count = collection.count_documents({"method" : "GET", "path" : "/sataus"})
+    status_count = collection.count_documents({"method" : "GET", "path" : "/status"})
     print(f"{status_count} status check")
