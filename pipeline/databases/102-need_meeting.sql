e7ff1a421235aa87f3e8452eb02b0918ebea4f84
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-26 20:17:53 +0200

    MO

--- pipeline/databases/102-need_meeting.sql
+++ pipeline/databases/102-need_meeting.sql
@@ -1,2 +1,3 @@
+--creates a view need_meeting that lists all students
 SELECT score,last_meeting FROM  students AS new_meeting 
 WHERE score<80 and (last_meeting IS NULL  OR  last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
