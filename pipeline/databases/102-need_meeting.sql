1b573a717aaf3bdd24838aadaf9b8d011edf598f
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-26 20:33:23 +0200

    MO

--- pipeline/databases/102-need_meeting.sql
+++ pipeline/databases/102-need_meeting.sql
@@ -1,9 +1,9 @@
---create a view 
-CREATE VIEW need_meeting AS
+-- Create or replace the view need_meeting
+CREATE OR REPLACE VIEW need_meeting AS
 SELECT name
-FROM students 
-WHERE score < 80 
-  AND (
-        last_meeting IS NULL 
-        OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
-      );
+FROM students
+WHERE score < 80
+AND (
+    last_meeting IS NULL
+    OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+);
