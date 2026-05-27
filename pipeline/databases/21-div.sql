1386e28cede3251ab0d7c835b13192a1c0f496a1
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-27 16:39:16 +0200

    mo

--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -6,11 +6,15 @@ CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
 DETERMINISTIC
 BEGIN
+    DECLARE result FLOAT;
+
     IF b = 0 THEN
-        RETURN 0;
+        SET result = 0;
+    ELSE
+        SET result = a / b;
     END IF;
 
-    RETURN a / b;
+    RETURN result;
 END //
 
 DELIMITER ;
