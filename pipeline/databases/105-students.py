5c00357be59bcbce767303d3e32baa7c8db9e443
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-27 21:14:00 +0200

    mo

--- pipeline/databases/105-students.py
+++ pipeline/databases/105-students.py
@@ -2,17 +2,11 @@
 
 """top students """
 
-
 def top_students(mongo_collection):
-    """
-    Returns all students sorted by average score (descending),
-    with 'averageScore' included in each document.
-    """
-
     pipeline = [
         {
             "$addFields": {
-                "averageScore": {"$avg": "$scores.score"}
+                "averageScore": {"$avg": "$scores"}
             }
         },
         {
