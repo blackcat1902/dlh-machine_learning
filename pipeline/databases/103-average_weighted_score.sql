17722f19d3354c415b8a442ee2036c8eeedaacbf
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-27 21:05:07 +0200

    mo

--- pipeline/databases/103-average_weighted_score.sql
+++ pipeline/databases/103-average_weighted_score.sql
@@ -1,21 +1,19 @@
 --ComputeAverageScoreForUse
 
-DELIMITER $$
-
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+
+DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
     UPDATE users
     SET average_score = (
         SELECT SUM(c.score * p.weight) / SUM(p.weight)
-        FROM corrections AS c
-        INNER JOIN projects AS p ON c.project_id = p.id
+        FROM corrections c
+        JOIN projects p ON c.project_id = p.id
         WHERE c.user_id = user_id
     )
     WHERE id = user_id;
 END $$
 
 DELIMITER ;
-
-CALL ComputeAverageWeightedScoreForUser(1);
