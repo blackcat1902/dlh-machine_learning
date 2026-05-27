6dc579e2d5c3f76fc5418c3c10c5bfeabcb0216c
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-27 21:45:33 +0200

    105

--- pipeline/databases/105-students.py
+++ pipeline/databases/105-students.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-
-"""top students """
+"""Top students."""
 
 def top_students(mongo_collection):
-    pipeline = [
-        {
-            "$addFields": {
-                "averageScore": {"$avg": "$scores"}
-            }
-        },
-        {
-            "$sort": {"averageScore": -1}
-        }
-    ]
+    """
+    Return all students sorted by average score.
 
+    Args:
+        mongo_collection: pymongo collection object
+
+    Returns:
+        List of students with averageScore field, sorted descending.
+    """
+    pipeline = [
+        {"$addFields": {"averageScore": {"$avg": "$topics.score"}}},
+        {"$sort": {"averageScore": -1}}
+    ]
     return list(mongo_collection.aggregate(pipeline))
