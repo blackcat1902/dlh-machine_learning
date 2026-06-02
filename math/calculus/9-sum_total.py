7b5ef13de91d3696046e3c2152d083084a170e4f
blackcat1902 <fatmacankutuk@gmail.com>
2026-06-02 22:07:52 +0200

    mo

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+""" the calculation sum of the squared numbers """
+
 def summation_i_squared(n):
     """ a function that calculates"""
-    if not isinstance(n, int) or isinstance(n, bool) or n<0:
+
+    if not isinstance(n, int)  or  n<0:
         return None
+    
     return n*(n+1)*(2*n+1)//6
