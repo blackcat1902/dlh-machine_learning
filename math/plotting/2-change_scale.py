5e3c58ad1dc9c62a56a864431853d6636811126e
blackcat1902 <fatmacankutuk@gmail.com>
2026-06-04 15:15:09 +0200

    mo

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def change_scale():
     """The function represents the cahnge of scale."""
 
@@ -12,7 +13,6 @@ def change_scale():
     t = 5730
     y = np.exp((r / t) * x)
     plt.figure(figsize=(6.4, 4.8))
-    
     plt.xlabel("Time (years)")
     plt.ylabel("Fraction Remaining")
     plt.title("Exponential Decay of C-14")
