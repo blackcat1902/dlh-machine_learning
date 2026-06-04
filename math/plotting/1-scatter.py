3eee052049db2af67b845106e731c82fe31f23cc
blackcat1902 <fatmacankutuk@gmail.com>
2026-06-04 14:48:30 +0200

    mo

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Module that displays  a scatter plot."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def scatter():
+    """ Dataset generation and plotting commands are defined here. """
+    
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.scatter(x,y, color='magenta')
+    plt.xlabel("Height (in)")
+    plt.ylabel("Weight (lbs)")
+    plt.title("Men's Height vs Weight")
+    plt.show()
