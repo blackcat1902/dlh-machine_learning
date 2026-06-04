89ff8cd015b8080c6a6cc05deac38c41041ddace
blackcat1902 <fatmacankutuk@gmail.com>
2026-06-04 17:32:36 +0200

    mo

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """Module that plots a histogram of student scores for a project."""
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def frequency():
     """The function that displays students scores for a project"""
-
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
