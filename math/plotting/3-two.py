07cffcaf38416bc5d795c613420479b3a93a20ef
blackcat1902 <fatmacankutuk@gmail.com>
2026-06-04 15:42:59 +0200

    mo

---
+++ math/plotting/3-two.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Module that displays a exponential decay of radioactive elements"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def two():
+    """The function represents that plot a line graphs """
+
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.xlabel("Time (years)")
+    plt.ylabel("Fraction Remaining")
+    plt.title("Exponential Decay of Radioactive Elements")
+    plt.xlim(0, 20.000)
+    plt.ylim(0,1)
+    plt.plot(x, y1, color='red', linestyle='dashed', label='C-14')
+    plt.plot(x,y2, color='green', linestyle='solid', label='Ra-226')
+    plt.legend(loc='upper right')
+    plt.show()
