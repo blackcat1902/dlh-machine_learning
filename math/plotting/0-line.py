c5528e230547c4ea66f5bf5a2f9747832bf366f7
blackcat1902 <fatmacankutuk@gmail.com>
2026-06-04 13:20:26 +0200

    mo

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
-plt.plot(x,y, color ='red', linestyle ='-')
-plt.xlim(0,10)
-plt.show()
+import numpy as np
+def line():
+    """Displays a cubic line graph"""
+    y= np.arange(0,11)**3
+    plt.figure(figsize=(6.4, 4.8))
+
+    x= np.arange(0,11)
+    plt.plot(x,y, color ='red', linestyle ='-')
+    plt.xlim(0,10)
+    plt.show()
