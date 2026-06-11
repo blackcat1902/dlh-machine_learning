178fedc0f57b285fe1405cf05ea8451c1cfc9bf5
blackcat1902 <fatmacankutuk@gmail.com>
2026-06-11 21:52:03 +0200

    mo

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -46,3 +46,30 @@ class Poisson:
 
         return pmf_value
     
+    def cdf(self, k):
+        """
+        Calculates the value of the CDF for a given number of 'successes'.
+        """
+        k = int(k)
+
+        if k < 0:
+            return 0
+
+        e = 2.7182818285
+        lambtha = self.lambtha
+        total_cdf = 0.0
+        factorial = 1
+
+        
+        for i in range(k + 1):
+            if i > 0:
+                factorial *= i
+
+            lambtha_power_i = lambtha ** i
+            e_power_lambtha = e ** lambtha
+            pmf_i = lambtha_power_i / (e_power_lambtha * factorial)
+
+            total_cdf += pmf_i
+
+        return total_cdf
+    
